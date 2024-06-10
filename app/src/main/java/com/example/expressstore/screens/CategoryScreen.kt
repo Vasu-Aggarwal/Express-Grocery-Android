fb6c01b075b0db450397f6153a5899fe7564e4f9
vasu.aggarwal <vasu.aggarwal@extramarks.com>
2024-06-10 18:06:45 +0530

    UI Changes in category

--- app/src/main/java/com/example/expressstore/screens/CategoryScreen.kt
+++ app/src/main/java/com/example/expressstore/screens/CategoryScreen.kt
@@ -2,12 +2,16 @@ package com.example.expressstore.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.expressstore.models.responses.CategoryDto
 import com.example.expressstore.utils.NetworkResult
@@ -16,12 +20,18 @@ import com.example.expressstore.viewmodels.CategoryViewModel
 @Composable
 fun CategoryScreen(categoryViewModel: CategoryViewModel = hiltViewModel()){
     val categories: State<NetworkResult<List<CategoryDto>>> = categoryViewModel.category.collectAsState()
-    LazyColumn {
+    LazyVerticalGrid (
+        columns = GridCells.Fixed(3)
+    ){
         categories.value.data?.let { categories ->
             items(categories){ category ->
                 CategoryItem(category = category)
+
             }
-            
+        } ?: run {
+            items(1){
+                Text(text = "Something went wrong")
+            }
         }
     }
 }
